dcad2f918d1dfa2162e0b5feeff5c1ca85c6e1a3
Diogo Sarti <diogosarti13@gmail.com>
2022-06-25 18:59:20 -0300

    tabuleiro-finalizado

---
+++ components/Tabuleiro.jsx
@@ -0,0 +1,16 @@
+import Linha from './Linha'
+
+export default function Tabuleiro(props){
+    return (
+        <>
+            <Linha />
+            <Linha preta />
+            <Linha />
+            <Linha preta />
+            <Linha />
+            <Linha preta />
+            <Linha />
+            <Linha preta />
+        </>
+    )
+}
